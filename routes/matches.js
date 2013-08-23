99f19b507b2a2e4123f40ba0e4185dc5eb0d44b4
Simen Johannessen <simenlomaa@gmail.com>
2013-08-23 16:08:04 +0200

    delete, all matches etc. added

--- routes/matches.js
+++ routes/matches.js
@@ -1,52 +1,100 @@
 var matchCollection = "matches"
 
-var MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server,
-    db;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/data');
 
-var mongoClient = new MongoClient(new Server('localhost', 27017));
-
-mongoClient.open(function(err, mongoClient) {
-    db = mongoClient.db("data");
-    db.collection(matchCollection, {strict:true}, function(err, collection) {
-        if (err) {
-            console.log("The " + matchCollection + " collection doesn't exist. Creating it with sample data...");
-            populateDB();
-        }
-    });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+    console.log("Yay open db")
 });
+
+var attacksSchema = mongoose.Schema({
+    matchId : Number,
+    match : String,
+    attacks : 
+        [
+            {
+                time : Number, 
+                passes : [
+                    {
+                        fromPlayer : Number, 
+                        toPlayer : Number, 
+                        fromPos : Number, 
+                        toPos : Number, 
+                        type : String
+                    }
+                ]
+            }
+        ]
+})
+
+attacksSchema.methods.speak = function () {
+
+};
+var Attacks = mongoose.model('Attacks', attacksSchema);
+
 
 exports.getAllMatches = function (req, res) {
     console.log("Find all matches");
-    db.collection(matchCollection, function(err, collection) {
-        collection.find().toArray(function(err, items) {
-            res.jsonp(items);
-            console.log(items);
-        });
-    });
+
+    Attacks.find(function (err, matches) {
+        if (err) res.send(400, {"msg" : "someting wrong happend during query executing"})
+        console.log("Session: %j", matches);
+        res.jsonp(matches);
+    })
 };
 
 exports.postNewMatch = function (req, res) {
     console.log("New match");
+    console.log(JSON.stringify(req.body));
 
-    var matchdata = req.body;
+    var matchdata = new Attacks(req.body);
 
-    console.log('New match rating');
-    console.log(JSON.stringify(matchdata));
-    
-    db.collection(matchCollection, function(err, collection) {
-        collection.insert(matchdata, {safe:true}, function(err, item) {
-              res.send(201, {"success" : true});
-        });
+    matchdata.save(function (err, fluffy) {
+        if (err){
+            res.send(400, {"msg" : "someting wrong happend during saving"})
+        }
+        else {
+            res.send(201, {"msg" : "success"});
+        }
     });
 };
 
 exports.postNewAttack = function (req, res) {
     console.log("New attack for match ");
+
 };
 
+exports.deleteAttack = function (req, res) {
+    matchId = parseInt(req.params.matchId)
+
+    Attacks.remove({ 'matchId': matchId }, function (err) {
+        if (err) res.send(400, {"msg" : "error deleting matchId"});
+
+        console.log("removeed")
+        res.send(200, {"msg" : "success"});
+    });
+}
+
 var populateDB = function() {
- 
+    
+    var MongoClient = require('mongodb').MongoClient,
+        Server = require('mongodb').Server,
+        db;
+
+    var mongoClient = new MongoClient(new Server('localhost', 27017));
+
+    mongoClient.open(function(err, mongoClient) {
+        db = mongoClient.db("data");
+        db.collection(matchCollection, {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The " + matchCollection + " collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    });
+
     console.log("Populating " + matchCollection + " database...");
     var matches = [
         {matchId : 1, match : "TIL - VIF", attacks :
@@ -64,9 +112,6 @@ var populateDB = function() {
                            fromPlayer : 15, fromPos : 19, toPos : 39, type : "SHOTMISS"
                         }
                     ]
-                },
-                {
-
                 }
             ]
         },
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.configure(function(){
 app.get('/matches', matches.getAllMatches); 
 app.post('/match', matches.postNewMatch);
 app.post('/match/:matchId/attack', matches.postNewAttack);
-
+app.del('/match/:matchId', matches.deleteAttack);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
