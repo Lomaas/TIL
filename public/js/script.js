1c85658c21671333df5a92a4f50c9ee3a7ea1276
Simen Johannessen <simenlomaa@gmail.com>
2013-08-27 18:04:52 +0200

    drawing circles on pitch

--- public/js/script.js
+++ public/js/script.js
@@ -11,17 +11,90 @@ var postNewMatchData = function(){
 	
 }
 
-// Creates canvas 320 × 200 at 10, 50
-var paper = Raphael(10, 50, 320, 200);
+// Creates canvas
+var paper = Raphael(10, 50, 1091, 759);
+var pitchImage = paper.image('/img/pitch.png', 0, 0, 1091, 759);
 
-// Creates circle at x = 50, y = 40, with radius 10
-var circle = paper.circle(50, 40, 10);
-// Sets the fill attribute of the circle to red (#f00)
-circle.attr("fill", "#f00");
+jQuery.ajax("matches/1/attack/9", {
 
-// Sets the stroke attribute of the circle to white
-circle.attr("stroke", "#fff");
+});
 
-var anim = Raphael.animation({cx: 10, cy: 20}, 2e3);
-circle1.animate(anim); // run the given animation immediately
-circle2.animate(anim.delay(500)); // run the given animation after 500 ms
+var numPlayers = 3;
+var counter;
+var adjustment = 50;
+
+var sonePlayer1 = 10;
+var sonePlayer2 = 14;
+var sonePlayer3 = 17;
+
+
+var zonesDictX = {
+	10 : 3,
+	7 : 2,
+	4 : 1,
+	1 : 0,
+	14 : 4,
+	17 : 5
+}
+
+var zonesDictY = {
+	10 : 0,
+	17 : 1,
+	14 : 1
+}
+
+var zonesX = 6;
+var zonesY = 3;
+
+
+console.log(zonesDictY[sonePlayer1]);
+
+var players = [
+	{
+		type: "circle",
+		cx: 1091/zonesX * zonesDictX[sonePlayer1] + adjustment,
+		cy: 759/zonesY * zonesDictY[sonePlayer1] + adjustment,
+		r: 10,
+		fill : '#f00',
+		stroke: "#fff"
+	},
+	{
+		type: "circle",
+		cx: 1091/zonesX * zonesDictX[sonePlayer2] + adjustment,
+		cy: 759/zonesY * zonesDictY[sonePlayer2] + adjustment,
+		r: 10,
+		fill : '#f00',
+		stroke: "#fff"
+	},
+	{
+		type: "circle",
+		cx: 1091/zonesX * zonesDictX[sonePlayer3] + adjustment,
+		cy: 759/zonesY * zonesDictY[sonePlayer3] + adjustment,
+		r: 10,
+		fill : '#f00',
+		stroke: "#fff"
+	}
+]
+
+var lines = [
+	{type : 'path'
+}
+]
+
+//paper.path("M 250 250 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z");
+for(counter = 0; counter < numPlayers; counter++){
+	paper.add(players)
+
+	if(counter + 1 != numPlayers){
+		console.log(counter)
+		console.log(players[counter]);
+		posX = players[counter].cx;
+		posY = players[counter].cy;
+		nextCoordX = players[counter + 1].cx - posX; 
+		nextCoordY = players[counter + 1].cy - posY;
+
+		console.log("M " + posX.toString()  + " " + posY.toString()  + " l " + nextCoordX.toString()  + " " + nextCoordY.toString())
+
+		var c = paper.path("M " + posX.toString()  + " " + posY.toString()  + " l " + nextCoordX.toString()  + " " + nextCoordY.toString());
+	}
+}
--- test/test.js
+++ test/test.js
@@ -5,49 +5,49 @@ var expect = require('expect.js');
 
 
 describe('Post a new match', function(){
-  // it("should return 201 success", function(done){
-  // 	request
-  // 		.post('http://localhost:3000/match')
-  // 		.send({
-		//     "matchId": 1,
-		//     "match": "TIL - VIF",
-		//     "attacks": [
-		//         {
-		//             "time": 20,
-		//             "passes": [
-		//                 {
-		//                     "fromPlayer": 4,
-		//                     "toPlayer": 10,
-		//                     "fromPos": 19,
-		//                     "toPos": 20,
-		//                     "type": "PASS"
-		//                 },
-		//                 {
-		//                     "fromPlayer": 10,
-		//                     "toPlayer": 15,
-		//                     "fromPos": 19,
-		//                     "toPos": 20,
-		//                     "type": "PASS"
-		//                 },
-		//                 {
-		//                     "fromPlayer": 15,
-		//                     "toPlayer": 19,
-		//                     "fromPos": 27,
-		//                     "toPos": 32,
-		//                     "type": "SHOTMISS"
-		//                 }
-		//             ]
-		//         }
-		//     ]
-		// })
-		// .set('Accept', 'application/json')
-  // 		.end(function(res){
-		// 	console.log(res.body);
-	 //    	expect(res).to.exist;
-		// 	expect(res.status).to.equal(201);
-		// 	done();
-  // 		});
-  // 	});
+  it("should return 201 success", function(done){
+  	request
+  		.post('http://localhost:3000/match')
+  		.send({
+		    "matchId": 1,
+		    "match": "TIL - VIF",
+		    "attacks": [
+		        {
+		            "time": 20,
+		            "passes": [
+		                {
+		                    "fromPlayer": 4,
+		                    "toPlayer": 10,
+		                    "fromPos": 19,
+		                    "toPos": 20,
+		                    "type": "PASS"
+		                },
+		                {
+		                    "fromPlayer": 10,
+		                    "toPlayer": 15,
+		                    "fromPos": 19,
+		                    "toPos": 20,
+		                    "type": "PASS"
+		                },
+		                {
+		                    "fromPlayer": 15,
+		                    "toPlayer": 19,
+		                    "fromPos": 27,
+		                    "toPos": 32,
+		                    "type": "SHOTMISS"
+		                }
+		            ]
+		        }
+		    ]
+		})
+		.set('Accept', 'application/json')
+  		.end(function(res){
+			console.log(res.body);
+	    	expect(res).to.exist;
+			expect(res.status).to.equal(201);
+			done();
+  		});
+  	});
 
 	it("should return 201 success", function(done){
   	request
@@ -94,26 +94,26 @@ describe('Post a new match', function(){
 
 
 
-// describe('Get all matches', function(){
-//   it("should return a 200", function(done){
-//   	request.get('http://localhost:3000/matches').end(function(res){
-//     	expect(res).to.exist;
-// 		expect(res.status).to.equal(200);
-// 		console.log(JSON.stringify(res.body));
-// 		//should.exist([])
-// 		//expect(res.body).to.contain('match:');
-// 		done();
-//   	});
-//   });
-// });
+describe('Get all matches', function(){
+  it("should return a 200", function(done){
+  	request.get('http://localhost:3000/matches').end(function(res){
+    	expect(res).to.exist;
+		expect(res.status).to.equal(200);
+		console.log(JSON.stringify(res.body));
+		//should.exist([])
+		//expect(res.body).to.contain('match:');
+		done();
+  	});
+  });
+});
 
-// describe('Delete a match', function(){
-// 	var id = 1; //TODO FIX!
-// 	it("should return a 200 on successful delete", function(done){
-// 		request.del('http://localhost:3000/match/' + id).end(function(res){
-// 		expect(res).to.exist;
-// 		expect(res.status).to.equal(200);
-// 		done();
-// 		});
-// 	});
-// });
+describe('Delete a match', function(){
+	var id = 1; //TODO FIX!
+	it("should return a 200 on successful delete", function(done){
+		request.del('http://localhost:3000/match/' + id).end(function(res){
+		expect(res).to.exist;
+		expect(res.status).to.equal(200);
+		done();
+		});
+	});
+});
