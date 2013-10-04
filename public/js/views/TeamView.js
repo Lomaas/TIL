d970a9863228bb50a56b9af5a261bfaf19d0c7b7
Simen Johannessen <simenlomaa@gmail.com>
2013-10-04 18:14:54 +0200

    fixed queries. small fixes in match/team view

--- public/js/views/TeamView.js
+++ public/js/views/TeamView.js
@@ -53,36 +53,38 @@ window.TeamView = Backbone.View.extend({
         var pitchSizeX = pictureSizeX - adjustment*2;
         var pitchSizeY = pictureSizeY - adjustment*2;
 
-        _.each(newArray, function(tmp){
+        [newArray[0]].every(function(tmp){
             jQuery.ajax({
                 url: "player/" + tmp.player_id,
                 dataType : "json",
                 success: function(resp,textStatus, jqXHR ){
                     console.log("RESPONSE %j", resp);
-                    
                 }
             });
 
 
             console.log(tmp.player_id.toString());
-            var paper = Raphael(tmp.player_id.toString(), "40%", "25%");
-            var pitchImage = paper.image('/img/emptyPitchSmall.png', 0, 0, pictureSizeX, pictureSizeY);
+            var paper = Raphael(tmp.player_id.toString(), "100%", "25%");
+            var pitchImage = paper.image('/img/pitchSmall.png', 0, 0, pictureSizeX, pictureSizeY);
 
             // Creates circle at x = 50, y = 40, with radius 10
-            var circle1 = paper.circle(50, 40, 10);
+            var circle1 = paper.circle(pictureSizeX/2, 30, 5);
             // Sets the fill attribute of the circle to red (#f00)
             circle1.attr("fill", "#f00");
             // Sets the stroke attribute of the circle to white
             circle1.attr("stroke", "#fff");
-            var circle2 = paper.circle(50, 40, 10);
+            var circle2 = paper.circle(pictureSizeX/2, pictureSizeY/2, 5);
             // Sets the fill attribute of the circle to red (#f00)
             circle2.attr("fill", "#f00");
             // Sets the stroke attribute of the circle to white
             circle2.attr("stroke", "#fff");
 
-            var anim = Raphael.animation({cx: 10, cy: 20}, 2e3);
-            circle1.animate(anim); // run the given animation immediately
-            circle2.animate(anim.delay(500)); // run the given animation after 500 ms
+            var animCircle1 = Raphael.animation({cx: pictureSizeX - 20, cy: 30}, 2e3);
+            var animCircle2 = Raphael.animation({cx: pictureSizeX - 20, cy: pictureSizeY/2 - 30}, 2e3);
+
+            circle1.animate(animCircle1); // run the given animation immediately
+            circle2.animate(animCircle2.delay(500)); // run the given animation after 500 ms
+
         });
 
 
